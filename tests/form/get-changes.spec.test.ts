d346f53f483e256d02d5880d445b3dede4eef2cc
jenesius <lokargenia@gmail.com>
2022-07-21 18:23:55 +0300

    Add test for form.changed

---
+++ tests/form/get-changes.spec.test.ts
@@ -0,0 +1,30 @@
+import Form from "../../plugin/classes/Form";
+
+describe("Form.changes", () => {
+
+	test("Empty Object. Form without changes", () => {
+		const form = new Form();
+		expect(form.changes).toEqual({});
+	})
+
+	test("Empty Object. Form. with values, but without changes", () => {
+		const form = new Form();
+		form.setValues({
+			name: "Jenesius"
+		})
+		expect(form.changes).toEqual({})
+	})
+
+	test("Object with one fields. Form with changes", () => {
+		const form = new Form();
+		form.change({
+			name: "Jenesius"
+		})
+
+		expect(form.changes).toEqual({
+			name: "Jenesius"
+		})
+	})
+
+
+})
