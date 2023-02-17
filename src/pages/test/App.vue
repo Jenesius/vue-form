0d5ebc42d1b64b6e3d5f50cde7a5921a1433a675
jenesius <lokargenia@gmail.com>
2023-02-18 01:32:24 +0300

    Code-review input-field

--- src/pages/test/App.vue
+++ src/pages/test/App.vue
@@ -1,18 +1,27 @@
 <template>
 	<div class="container-examples">
 		<input-field type = "select" label = "Country" :options = "optionsCountry" name = "Country"/>
-		<input-field type = "select" label = "City" :options = "optionsCity" name = "City"/>
+
+		<input-field type = "select" label = "City" :options = "optionsCity" name = "City" v-model = "inputModelValue"/>
+		<input-field type = "select" label = "City" :options = "optionsCity"  v-model = "inputModelValue"/>
+
 		<input-field type = "select" label = "City" :options = "longList" name = "City2" placeholder = "Select City"/>
 		<input-field type = "select" label = "City" :options = "[]" name = "City1" placeholder = "Select"/>
+		<input v-model = "inputModelValue"/>
+		{{inputModelValue}}
+		{{values}}
 	</div>
 </template>
 
 <script setup lang='ts'>
 import InputField from "../../../plugin/widgets/input-field.vue";
-import {Form} from "../../../plugin";
-import {reactive} from "vue";
+import {Form, useFormValues} from "../../../plugin";
+import {reactive, ref} from "vue";
 import {OptionRow} from "../../../plugin/types";
 const form = new Form();
+const values = useFormValues(form)
+
+const inputModelValue = ref('')
 
 const optionsCountry = {
 	1: 'USA',
@@ -39,6 +48,9 @@ for(let i = 0; i < 1000; i++) {
 		value: i,
 	})
 }
+
+// @ts-ignore
+window.form = form
 
 </script>
 
