fb9c0ea26a75f89f7664449386a203240a3f020e
jenesius <lokargenia@gmail.com>
2023-03-30 13:40:50 +0300

    Add param TypeNotCaseSensitive. Add tests. Resolve #92.

--- plugin/config/config.ts
+++ plugin/config/config.ts
@@ -1,18 +1,31 @@
 import mergeObjects from "../utils/merge-objects";
-import STORE from "./store";
+import STORE, {IStore} from "./store";
+import Store from "./store";
 
 export default function config(params: ConfigParams) {
 
 	/**
 	 * In case if params includes inputTypes, merge provided component with default widgets.
 	 */
-	if (params.inputTypes)
-		mergeObjects(STORE.inputTypes, params.inputTypes)
-	
+	if ("typeNotCaseSensitive" in params && typeof params.typeNotCaseSensitive === "boolean") Store.typeNotCaseSensitive = params.typeNotCaseSensitive;
+
+	try {
+		if (params.inputTypes) {
+			let parsedInputTypes = params.inputTypes;
+
+			if (STORE.typeNotCaseSensitive)
+				parsedInputTypes = Object.entries(params.inputTypes).reduce<Record<string, any>>((acc, [type, component]) => {
+					acc[type.toLowerCase()] = component
+					return acc;
+				}, {})
+
+			mergeObjects(STORE.inputTypes, parsedInputTypes)
+		}
+	} catch (e) {
+		console.error(e)
+	}
 }
 
-interface ConfigParams {
-	inputTypes?: {
-		[name: string]: any
-	}
+type ConfigParams = Partial<IStore> & {
+	inputTypes?: IStore['inputTypes']
 }
