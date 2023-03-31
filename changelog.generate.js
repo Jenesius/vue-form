0681f007f23f31cfdc5a7e458804262618266a61
jenesius <lokargenia@gmail.com>
2023-04-01 01:59:09 +0300

    fix: remove changelog-generate file

--- changelog.generate.js
+++
@@ -1,8 +0,0 @@
-const changelog = require('generate-changelog');
-const fs        = require('fs');
-const pkg = require('./package.json')
-
-changelog.generate({ patch: true, repoUrl: /(.*)(\.git)/.exec(pkg.repository.url)[1] })
-.then(function (changelog) {
-    fs.writeFileSync('./CHANGELOG.md', changelog);
-});
